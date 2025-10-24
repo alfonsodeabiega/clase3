46e6d03f4eaf85e989bf6551d3a8d46cd770530a
alfonsodeabiega <alfonso.deabiega@gmail.com>
2025-10-24 13:50:21 -0600

    Create ejercicio2.c

---
+++ ejercicio2.c
@@ -0,0 +1,12 @@
+/*Alfonso Francisco De Abiega L'Eglisse
+clase 3 ejercicio 2*/
+
+#include <stdio.h>
+
+void main()
+{
+  for(int i = 1; i <= 10; i++)
+    {
+      printf("Num: %d\n", i);
+    }
+}
