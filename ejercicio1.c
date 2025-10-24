5b2ae139c3a16e81e226248aff3845475e792d06
alfonsodeabiega <alfonso.deabiega@gmail.com>
2025-10-24 13:40:44 -0600

    Create ejercicio1.c

---
+++ ejercicio1.c
@@ -0,0 +1,24 @@
+/*Alfonso Francisco De Abiega L'Eglisse
+clase 3 ejercicio 1*/
+
+#include <stdio.h>
+#include <math.h>
+
+void main()
+{
+  int x = 0, res = 0;
+
+  printf("Dame un numero: "):
+  scanf("%d", &x);
+
+  if (x <= 0)
+  {
+    res = x + 3;
+  }
+  else
+  {
+    res = pow(x,2) + (x * 2);
+  }
+
+  printf("El resultado es: %d\n", res);
+}
